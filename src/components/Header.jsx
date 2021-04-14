965c8a20eb0dcf5483a2da2540e3246df1c6190d
Janssen <jeanbaptiste.janssen10@gmail.com>
2021-04-14 15:09:00 +0200

    fichier maj

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 import React, { useState } from 'react';
 import { ReactComponent as CloseMenu } from '../assets/x.svg';
 import { ReactComponent as MenuIcon } from '../assets/menu.svg';
@@ -7,47 +8,76 @@ import './header.css';
 const Header = () => {
   const [click, setClick] = useState(false);
   const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
   return (
     <div className="header">
       <div className="logo-nav">
         <div className="logo-container">
-          <div className="mobile-menu" onClick={handleClick}>
-            {click ? (
-              <CloseMenu className="menu-icon" />
-            ) : (
-              <MenuIcon className="menu-icon" />
-            )}
-          </div>
-          <img
-            className="logo-voiture"
-            src="/images/picto-voiture-removebg.png"
-            alt="logo voiture"
-          />
-          <img
-            className="logo-marguerite"
-            src="/images/Logo_Marguerite-removebg.png"
-            alt="Logo marguerite"
-          />
-          <ul className={click ? 'nav-options active' : 'nav-options'}>
-            <li className="option">
-              <a href="/">Home</a>
-            </li>
-            <li className="option">
-              <a href="/">Mode demploi</a>
-            </li>
-            <li className="option">
-              <a href="/">Réservation marguerite</a>
-            </li>
-            <li className="option mobile-option">
-              <a href="/">Contact</a>
-            </li>
-          </ul>
-          <img className="devise" src="/images/devise.jpg" alt="devise" />
+          <a href="/">
+            <Logo className="logo" />
+          </a>
         </div>
+        <ul className={click ? 'nav-options active' : 'nav-options'}>
+          <li
+            className="option"
+            onClick={closeMobileMenu}
+            onKeyPress={closeMobileMenu}
+          >
+            <a href="/">ABOUT</a>
+          </li>
+          <li
+            className="option"
+            onClick={closeMobileMenu}
+            onKeyPress={closeMobileMenu}
+          >
+            <a href="/">CONTACT</a>
+          </li>
+          <li
+            className="option"
+            onClick={closeMobileMenu}
+            onKeyPress={closeMobileMenu}
+          >
+            <a href="/">BLOG</a>
+          </li>
+          <li
+            className="option mobile-option"
+            onClick={closeMobileMenu}
+            onKeyPress={closeMobileMenu}
+          >
+            <a href="/">SIGN-IN</a>
+          </li>
+          <li
+            className="option mobile-option"
+            onClick={closeMobileMenu}
+            onKeyPress={closeMobileMenu}
+          >
+            <a href="/" className="sign-up">
+              SIGN-UP
+            </a>
+          </li>
+        </ul>
       </div>
-      <a href="/">
-        <Logo className="logo" />
-      </a>
+      <ul className="signin-up">
+        <li
+          className="sign-in"
+          onClick={closeMobileMenu}
+          onKeyPress={closeMobileMenu}
+        >
+          <a href="/">SIGN-IN</a>
+        </li>
+        <li onClick={closeMobileMenu} onKeyPress={closeMobileMenu}>
+          <a href="/" className="signup-btn">
+            SIGN-UP
+          </a>
+        </li>
+      </ul>
+      <button type="button" className="mobile-menu" onClick={handleClick}>
+        {click ? (
+          <CloseMenu className="menu-icon" />
+        ) : (
+          <MenuIcon className="menu-icon" />
+        )}
+      </button>
     </div>
   );
 };
