fe00e611a186e02a82e7f436d439d6f1367ed2d0
Janssen <jeanbaptiste.janssen10@gmail.com>
2021-04-15 17:12:33 +0200

    Update headerjsx et headercss

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,45 +13,51 @@ const Header = () => {
     <div className="header">
       <div className="logo-nav">
         <div className="logo-container">
-          <div className="mobile-menu" onClick={handleClick}>
-            {click ? (
-              <CloseMenu className="menu-icon" />
-            ) : (
-              <MenuIcon className="menu-icon" />
-            )}
+          <div className="header-top">
+            <div className="dispoLogo">
+              <div className="mobile-menu" onClick={handleClick}>
+                {click ? (
+                  <CloseMenu className="menu-icon" />
+                ) : (
+                  <MenuIcon className="menu-icon" />
+                )}
+              </div>
+              <img
+                className="logo-voiture"
+                src="/images/picto-voiture-removebg.png"
+                alt="logo voiture"
+              />
+            </div>
+            <img
+              className="logo-marguerite"
+              src="/images/Logo_Marguerite-removebg.png"
+              alt="Logo marguerite"
+            />
+            <ul className={click ? 'nav-options active' : 'nav-options'}>
+              <li className="option">
+                <a href="/">Home</a>
+              </li>
+              <li className="option">
+                <a href="/">Mode demploi</a>
+              </li>
+              <li className="option">
+                <a href="/">Réservation marguerite</a>
+              </li>
+              <li className="option mobile-option">
+                <a href="/">Contact</a>
+              </li>
+            </ul>
+            <img
+              className="logo-profil"
+              src="/images/logo-profil-removebg.png"
+              alt="logo-profil"
+            />
           </div>
-          <img
-            className="logo-voiture"
-            src="/images/picto-voiture-removebg.png"
-            alt="logo voiture"
-          />
-          <img
-            className="logo-marguerite"
-            src="/images/Logo_Marguerite-removebg.png"
-            alt="Logo marguerite"
-          />
-          <ul className={click ? 'nav-options active' : 'nav-options'}>
-            <li className="option">
-              <a href="/">Home</a>
-            </li>
-            <li className="option">
-              <a href="/">Mode demploi</a>
-            </li>
-            <li className="option">
-              <a href="/">Réservation marguerite</a>
-            </li>
-            <li className="option mobile-option">
-              <a href="/">Contact</a>
-            </li>
-          </ul>
-          <img className="devise" src="/images/devise.jpg" alt="devise" />
+          <div className="header-bottom">
+            <img className="devise" src="/images/devise.jpg" alt="devise" />
+          </div>
         </div>
       </div>
-      <img
-        className="logo-profil"
-        src="/images/logo-profil-removebg.png"
-        alt="logo-profil"
-      />
     </div>
   );
 };
