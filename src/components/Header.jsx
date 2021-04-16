2672657fd701e80a27ef14ce6addade42139a87a
bastienwcs <bastien.krettly@wildcodeschool.com>
2021-04-16 11:02:56 +0200

    css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,58 +11,54 @@ const Header = () => {
   const handleClick = () => setClick(!click);
   return (
     <div className="header">
-      <div className="logo-nav">
-        <div className="logo-container">
-          <div className="header-top">
-            <div className="dispoLogo">
-              <div className="mobile-menu" onClick={handleClick}>
-                {click ? (
-                  <CloseMenu className="menu-icon" />
-                ) : (
-                  <MenuIcon className="menu-icon" />
-                )}
-              </div>
-              <img
-                className="logo-voiture"
-                src="/images/picto-voiture-removebg.png"
-                alt="logo voiture"
-              />
-            </div>
-            <img
-              className="logo-marguerite"
-              src="/images/Logo_Marguerite-removebg.png"
-              alt="Logo marguerite"
-            />
-
-            <ul className={click ? 'nav-options active' : 'nav-options'}>
-              <li className="option">
-                <a href="/">Home</a>
-              </li>
-              <li className="option">
-                <a href="/">Mode demploi</a>
-              </li>
-              <li className="option">
-                <a href="/">Réservation marguerite</a>
-              </li>
-              <li className="option mobile-option">
-                <a href="/">Contact</a>
-              </li>
-            </ul>
-            <img
-              className="logo-profil"
-              src="/images/logo-profil-removebg.png"
-              alt="logo-profil"
-            />
+      <div className="header-top">
+        <div className="dispoLogo">
+          <div className="mobile-menu" onClick={handleClick}>
+            {click ? (
+              <CloseMenu className="menu-icon" />
+            ) : (
+              <MenuIcon className="menu-icon" />
+            )}
           </div>
-          <div className="header-bottom">
-            <img
-              className="devise"
-              src="/images/devise-removebg.png"
-              alt="devise"
-            />
-          </div>
+          <img
+            className="logo-voiture"
+            src="/images/picto-voiture-removebg.png"
+            alt="logo voiture"
+          />
         </div>
+        <img
+          className="logo-marguerite"
+          src="/images/logo_Marguerite-removebg.png"
+          alt="Logo marguerite"
+        />
+
+        <img
+          className="logo-profil"
+          src="/images/logo-profil-removebg.png"
+          alt="logo-profil"
+        />
       </div>
+      <div className="header-bottom">
+        <img
+          className="devise"
+          src="/images/devise-removebg.png"
+          alt="devise"
+        />
+      </div>
+      <ul className={click ? 'nav-options active' : 'nav-options'}>
+        <li className="option">
+          <a href="/">Home</a>
+        </li>
+        <li className="option">
+          <a href="/">Mode demploi</a>
+        </li>
+        <li className="option">
+          <a href="/">Réservation marguerite</a>
+        </li>
+        <li className="option mobile-option">
+          <a href="/">Contact</a>
+        </li>
+      </ul>
     </div>
   );
 };
