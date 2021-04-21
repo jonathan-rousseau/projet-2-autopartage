c1f2397312131879d7bf4660f532940dece87ac5
bpichery <pichery.benedicte@gmail.com>
2021-04-21 14:33:35 +0200

    version ok sans interactivité

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -12,7 +12,9 @@ function Login() {
             src="https://cdn.discordapp.com/attachments/828980599762321419/834365801603268608/kisspng-computer-icons-christian-cross-icon-design-clip-ar-5afc2de4bc8555.6906750415264762607722-rem.png"
           />
         </a>
-        <div className="titre">Connexion</div>
+        <div id="connexion" className="titre">
+          Connexion
+        </div>
         <div className="titre">
           Accéder à votre compte <span>Marguerite</span>
         </div>
