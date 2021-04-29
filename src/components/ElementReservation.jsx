ca30f19dfcc31b8c86932663bcbfe215fe35a621
GUIGANTONDamien <guigantondamien@gmail.com>
2021-04-29 11:51:42 +0200

    Update bouton annuler + css

--- src/components/ElementReservation.jsx
+++ src/components/ElementReservation.jsx
@@ -1,19 +1,29 @@
-import React from 'react';
 import './profil.css';
 
 function ElementReservation(props) {
   // eslint-disable-next-line react/prop-types
-  const { commandDate, price, reservDate } = props;
+  const { commandDate, price, reservDate, id, disableReservation } = props;
+
+  function handleClick() {
+    disableReservation(id);
+  }
+
   return (
     <div className="white">
       <div id="inline">
-        <h3>Commande effectuée le {commandDate}</h3>
-        <div id="euros">Total {price} Euros</div>
+        <h3>
+          <span>Commande</span> effectuée le {commandDate}
+        </h3>
+        <div id="euros">Total {price} €</div>
       </div>
       <div id="inline">
-        <section id="reservation">Réservation pour le {reservDate}</section>
+        <section id="reservation">
+          <span>Réservation</span> pour le {reservDate}
+        </section>
         <h3>
-          <span>Annuler</span> la réservation
+          <button id="button-cancel" type="button" onClick={handleClick}>
+            Annuler
+          </button>
         </h3>
       </div>
     </div>
--- src/components/Profil.jsx
+++ src/components/Profil.jsx
@@ -1,41 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ElementReservation from './ElementReservation';
 import './profil.css';
 
-const infoReservation = [
-  {
-    commandDate: '01/01/2021',
-    price: 5,
-    reservDate: '99/99/9999',
-  },
-  {
-    commandDate: '15/05/2020',
-    price: 15,
-    reservDate: '25/03/2018',
-  },
-  {
-    commandDate: '20/12/2019',
-    price: 5,
-    reservDate: '03/03/2017',
-  },
-];
 function Profil() {
+  const [infoReservation, setInfoReservation] = useState([
+    {
+      commandDate: '01/01/2021',
+      price: 5,
+      reservDate: '99/99/9999',
+      active: true,
+      id: 1,
+    },
+    {
+      commandDate: '15/05/2020',
+      price: 15,
+      reservDate: '25/03/2018',
+      active: true,
+      id: 2,
+    },
+    {
+      commandDate: '20/12/2019',
+      price: 5,
+      reservDate: '03/03/2017',
+      active: true,
+      id: 3,
+    },
+  ]);
+  function disableReservation(id) {
+    const reservationFound = infoReservation.find(
+      (reservation) => reservation.id === id
+    );
+    reservationFound.active = false;
+    setInfoReservation([
+      ...infoReservation.filter((r) => r.id !== id),
+      reservationFound,
+    ]);
+  }
   return (
     <div>
       <div id="connexion">Se déconnecter</div>
       <div>Jean Benguigui</div>
       <section id="wrapper">
         <h1>Historique de réservations</h1>
-        {infoReservation.map((reservation) => (
-          <ElementReservation
-            commandDate={reservation.commandDate}
-            price={reservation.price}
-            reservDate={reservation.reservDate}
-          />
-        ))}
-
-        <ElementReservation />
-        <ElementReservation />
+        {infoReservation
+          .filter((info) => info.active === true)
+          .map((reservation) => (
+            <ElementReservation
+              commandDate={reservation.commandDate}
+              price={reservation.price}
+              reservDate={reservation.reservDate}
+              id={reservation.id}
+              disableReservation={disableReservation}
+            />
+          ))}
       </section>
 
       <section id="wrapper">
@@ -56,4 +73,8 @@ Tester que sa s'affiche bien dans le profil
 Envoyer les valeurs en props
 Créer un tableau d'objet de réservation et faire un map sur ce tableau pour afficher la liste des réservations
 Filter pour afficher ou cacher le composant (statelifting)
+
+
+
+
 */
