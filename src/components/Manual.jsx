7f250abedbaa24c6f8565e8a29a37c30e9062550
Jb10-0 <74372841+JbWCS@users.noreply.github.com>
2021-05-05 15:50:45 +0200

    création composant règles

--- src/components/Manual.jsx
+++ src/components/Manual.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import './manual.css';
 
 function Manual() {
   return (
-    <div>
-      <h1>Manual</h1>
+    <div className="carré-container">
+      <h1>Guide du bon conducteur</h1>
+      <div className="carré">
+        <div className="stylecarrée">
+          <h1>I-La vitesse, tu limiteras</h1>
+          <h2>II- L&apos; alcool / stupéfiants, tu bannieras</h2>
+          <h3>III- Le téléphone, tu le poseras</h3>
+          <h4>IV-Les mains sur le volant, tu garderas</h4>
+          <h5>V-Les bonnes manières sur la route, tu auras</h5>
+          <h6>VI- La distance de sécurité, tu garderas</h6>
+        </div>
+      </div>
     </div>
   );
 }
