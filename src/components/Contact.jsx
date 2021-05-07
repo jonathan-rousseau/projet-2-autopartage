baa275e08225766004702003d110d3a27fd7a9dc
GUIGANTONDamien <guigantondamien@gmail.com>
2021-05-07 10:02:58 +0200

    Création page contact + modifications interne

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,35 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
+import './contact.css';
 
 function Contact() {
   return (
     <div>
-      <h1>Contact</h1>
+      <div id="letter">Vous avez des questions?</div>
+      <section className="contact">
+        <div className="bloc">
+          <form id="contactForm">
+            <label htmlFor="name">Votre nom et prénom:</label>
+            <br />
+            <input type="text" id="name" name="name" placeholder="NOM Prénom" />
+            <br />
+            <label htmlFor="email">Votre adresse mail:</label>
+            <br />
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="example@marguerite.com"
+            />
+            <br />
+            <label htmlFor="message">Votre message:</label>
+            <br />
+            <textarea id="message" name="message" />
+            <br />
+            <input id="submit" type="submit" value="Envoyer" />
+          </form>
+        </div>
+      </section>
     </div>
   );
 }
